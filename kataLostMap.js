e58300af1e77b8dec9c92b39a81c0b8338c45833
gary <gary.white@lodgit.com>
2021-09-28 12:53:33 +0200

    working, not perfect

--- kataLostMap.js
+++ kataLostMap.js
@@ -1,12 +1,27 @@
-function maps(x){
-    var arr = [''];
-    for(var i = 0; i < x.length; i++) {
+// function maps(x){
+//     var arr = [''];
+//     for(var i = 0; i < x.length; i++) {
+//         for(i = 0; i < x[i]; i++){
+//             x[i] = x[i] * 2;
+//         }
         // console.log(x[i])
         // arr = x[i] * x[i];
         // return arr;
-        arr = arr + x[i];
-        console.log(arr)
+        // arr = arr + x[i];
+         
+//     }
+//     console.log(x)
+// }
+
+function maps(x){
+    var arr = [''];
+    for(var i = 0; i < x.length; i++) {
+        x[i] = x[i] * 2;
     }
+    console.log(x)
 }
 
 console.log(maps([1, 2, 3])); // returns [2, 4, 6]
+console.log(maps([2, 4, 6])); // returns [4, 8, 12]
+console.log(maps([4, 1, 1, 1, 4])); // returns [8, 2, 2, 2, 8]
+// console.log(maps([2, 2, 2, 2, 2, 2])); // returns [4, 4, 4, 4, 4, 4]
